dda16f2d5e21f918a93657e61d24e04d6ad661ed
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2026-03-28 23:09:41 -0300

    GameScout: MyApi se dejo consumiendo la API, se maqueteo con una cadr que se importa, se instalo bootstrap, se coloco responsive

--- src/components/MyApi.jsx
+++ src/components/MyApi.jsx
@@ -1,33 +1,37 @@
-// src/components/MyApi.jsx
-
 import { useEffect, useState } from 'react';
 
-import Card from './Card'; // Importamos tu nuevo componente
+import Card from './Card';
+import axios from 'axios';
 
 function MyApi() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        const apiUrl = 'https://corsproxy.io/?https://www.freetogame.com/api/games';
 
-        fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) throw new Error('Network response was not ok');
-            return response.json();
-        })
-        .then(data => setData(data))
-        .catch(error => console.error("Error consumiendo la API:", error));
+        const getGames = async () => {
+            try {
+                const apiUrl = 'https://corsproxy.io/?https://www.freetogame.com/api/games';
+                
+                const response = await axios.get(apiUrl);
+
+                setData(response.data); 
+
+            } catch (error) {
+                console.error("Error consumiendo la API:", error);
+            }
+        };
+
+        getGames();
+        
     }, []);
 
     return (
         <div className="container mt-4">
             <h2 className="text-center mb-4">Catálogo de Juegos Gratuitos</h2>
-            
-            {/* Grilla responsiva de Bootstrap */}
+
             <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
                 {data.map(game => (
                     <div className="col" key={game.id}>
-                        {/* Renderizamos tu componente Card pasándole los datos del juego */}
                         <Card game={game} />
                     </div>
                 ))}
